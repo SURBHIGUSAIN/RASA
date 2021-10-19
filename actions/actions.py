e0d15e327fba3133a48d951327ada87d491c3bc9
SURBHI GUSAIN <SURBHIGUSAIN74@GMAIL.COM>
2021-10-19 23:35:59 +0530

    RASA project1

---
+++ actions/actions.py
@@ -0,0 +1,123 @@
+# This files contains your custom actions which can be used to run
+# custom Python code.
+#
+# See this guide on how to implement these action:
+# https://rasa.com/docs/rasa/custom-actions
+
+
+# This is a simple example for a custom action which utters "Hello World!"
+
+# from typing import Any, Text, Dict, List
+#
+# from rasa_sdk import Action, Tracker
+# from rasa_sdk.executor import CollectingDispatcher
+#
+#
+# class ActionHelloWorld(Action):
+#
+#     def name(self) -> Text:
+#         return "action_hello_world"
+#
+#     def run(self, dispatcher: CollectingDispatcher,
+#             tracker: Tracker,
+#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+#
+#         dispatcher.utter_message(text="Hello World!")
+#
+#         return []
+
+from typing import Text, List, Optional, Dict, Any
+
+from rasa_sdk import Tracker, Action
+from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk.forms import FormValidationAction
+from dotenv import load_dotenv
+import os
+import requests
+import json
+import uuid
+
+load_dotenv()
+
+airtable_api_key = os.getenv("AIRTABLE_API_KEY")
+base_id = os.getenv("BASE_ID")
+table_name = os.getenv("TABLE_NAME")
+
+
+def create_health_log(confirm_exercise, exercise, sleep, diet, stress, goal):
+    request_url = f"https://api.airtable.com/v0/{base_id}/{table_name}"
+
+    headers = {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+        "Authorization": f"Bearer {airtable_api_key}",
+    }
+
+    data = {
+        "fields": {
+            "Id": str(uuid.uuid4()),
+            "Exercised?": confirm_exercise,
+            "Type of exercise": exercise,
+            "Amount of sleep": sleep,
+            "Stress": stress,
+            "Diet": diet,
+            "Goal": goal,
+        }
+    }
+
+    print(request_url)
+    print(headers)
+    print(json.dumps(data))
+
+    try:
+        response = requests.post(
+            request_url, headers=headers, data=json.dumps(data)
+        )
+        response.raise_for_status()
+    except requests.exceptions.HTTPError as err:
+        raise SystemExit(err)
+
+    print(f"Response status code: {response.status_code}")
+    return response
+
+
+class ValidateHealthForm(FormValidationAction):
+    def name(self) -> Text:
+        return "validate_health_form"
+
+    async def required_slots(
+            self,
+            slots_mapped_in_domain: List[Text],
+            dispatcher: "CollectingDispatcher",
+            tracker: "Tracker",
+            domain: "DomainDict",
+    ) -> Optional[List[Text]]:
+        print(f"confirm_exercise is {tracker.get_slot('confirm_exercise')}")
+        if not tracker.get_slot("confirm_exercise"):
+            slots_mapped_in_domain.remove("exercise")
+
+        print(slots_mapped_in_domain)
+        return slots_mapped_in_domain
+
+
+class SubmitHealthForm(Action):
+
+    def name(self) -> Text:
+        return "submit_health_form"
+
+    async def run(
+            self, dispatcher, tracker: Tracker, domain: Dict[Text, Any],
+    ) -> List[Dict[Text, Any]]:
+        confirm_exercise = tracker.get_slot("confirm_exercise")
+        exercise = tracker.get_slot("exercise")
+        sleep = tracker.get_slot("sleep")
+        stress = tracker.get_slot("stress")
+        diet = tracker.get_slot("diet")
+        goal = tracker.get_slot("goal")
+
+        response = create_health_log(
+            confirm_exercise, exercise, sleep, stress, diet, goal)
+
+        dispatcher.utter_message("Thanks, your answers have been recorded!")
+
+        return []
